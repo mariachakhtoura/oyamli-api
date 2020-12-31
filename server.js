0a8870ea152ac1f398250ba24b3529c421559c03
MariaChakhtoura <maria.chakhtoura@aqeed.com>
2020-12-31 17:22:41 +0200

    MC - added access control

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import fetch from 'node-fetch';
 const app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
