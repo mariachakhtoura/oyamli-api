c2290a700835b4b56e19909d328bd1c4276b7055
MariaChakhtoura <maria.chakhtoura@aqeed.com>
2020-12-31 16:48:18 +0200

    MC - added api

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
-
+import fetch from 'node-fetch';
 const app = express();
 
-app.get('/oyamli/:key', (req, res) => {
-  var resut = [];
+app.post('/oyamli/:key', (req, res) => {
+  console.log(req.params);
+  var result = [];
+  var success = false;
   fetch(
-    `https://api.yamli.com/transliterate.ashx?word=${req.params.key.toString()}&tool=api&account_id=000006&prot=https%3A&hostname=www.yamli.com&path=%2Farabic-keyboard%2F&build=5515&sxhr_id=15`,
+    `https://api.yamli.com/transliterate.ashx?word=${req.params.key}&tool=api&account_id=000006&prot=https%3A&hostname=www.yamli.com&path=%2Farabic-keyboard%2F&build=5515&sxhr_id=15`,
     {
       headers: {
         'Accept-Language': 'en-US,en;q=0.5',
@@ -15,27 +17,30 @@ app.get('/oyamli/:key', (req, res) => {
       referrer: 'https://www.yamli.com/arabic-keyboard/',
     }
   )
-    .then((response) => response.json())
-    .then((response) => {
-      let items;
-      const matchedData = response
-        .toString()
-        .match(/(?<=\{"data":")(.*?)(?=",")/);
-      if (matchedData && matchedData.length > 0) {
-        // const itemObjects = JSON.parse( matchedData[ 0 ] );
-        // const rItems = JSON.parse( itemObjects.data ).r;
-        const rItems = matchedData[0];
-        const arabicChoices = rItems.match(/[\u0621-\u064A ]+/g);
-        const totalChoices = [queryText, ...arabicChoices];
-        // console.log( arabicChoices );
-        resut = totalChoices;
-      } else {
-        result = [];
-      }
+  .then(res => res.text())
+    .then(body => {
+        let items;
+            const matchedData = body
+              .toString()
+              .match(/(?<=\{"data":")(.*?)(?=",")/);
+              console.log(matchedData);
+            if (matchedData && matchedData.length > 0) {
+              // const itemObjects = JSON.parse( matchedData[ 0 ] );
+              // const rItems = JSON.parse( itemObjects.data ).r;
+              const rItems = matchedData[0];
+              const arabicChoices = rItems.match(/[\u0621-\u064A ]+/g);
+              const totalChoices = [req.params.key, ...arabicChoices];
+              result = totalChoices;
+              console.log(totalChoices);
+              success = true;
+            } else {
+              console.log([]);
+              success = false;
+            }
     }).finally(() => {
       res.status(200).send({
-        success: 'true',
-        result
+        success,
+        result,
       })
     });
 });
